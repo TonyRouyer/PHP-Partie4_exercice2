3c0c5496e2531887cdfda372ee62963539e9d106
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-08 15:47:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    function returnStr($str){
+            echo $str;
+        } 
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>partie 4 exercice 2</title>
+    </head>
+    <body>
+        <p><?php returnStr('bla bla bla') ?></p>
+    </body>
+</html>
